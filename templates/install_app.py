604c15d96c9f6e2604b7551fd0f4fb4cf1992bf5
Vladislav Gorbunov <vadikgo@gmail.com>
2016-03-03 19:06:43 +0300

    test nexus

--- templates/install_app.py
+++ templates/install_app.py
@@ -3,30 +3,40 @@ import re
 execfile("{{ovp_tmpdir}}/was_import/wasadminlib.py")
 
 ovp_clusters = {{ovp_clusters}}
+ovp_app_dist_names = {{ovp_app_dist_names}}
 
 app_cl = {}
 for cluster in ovp_clusters:
     for app in cluster["apps"]:
-        if app in app_cl:
+        if app in app_cl.keys():
             app_cl[app].append(cluster["name"])
         else:
             app_cl[app] = [cluster["name"]]
 
-installed_apps = listApplications():
-#installed_apps = [ "Ovp-Admin-15.3.0.war", "SchedulerTask-15.3.0-AD.jar" ]
+installed_apps = listApplications()
 
 for app in app_cl.keys():
-    for app_name in installed_apps:
-        if re.split("war|jar", app)[0] == re.split("war|jar", app_name)[0]:
-            print app, "already installed"
-            break
-        elif re.split("\d+\.\d+\.\d+\.", app)[0] == re.split("\d+\.\d+\.\d+\.", app_name)[0]:
-            print "update", app_name
-            updateApplication("{{ovp_tmpdir}}/%s" % app)
-            AdminConfig.save()
-            break
-        elif app not in [re.split("\d+\.\d+\.\d+\.", a)[0] for a in installed_apps]:
-            print "install", app, app_cl[app]
-            installApplication("{{ovp_tmpdir}}/%s" % app, [], [app_cl[app]], [])
-            AdminConfig.save()
-            break
+    appname = re.split("\.war|\.jar", app)[0]
+    if appname in [re.split("\.war|\.jar", a)[0] for a in installed_apps]:
+        print app, "already installed"
+    elif re.split("\d+\.\d+\.\d+\.", app)[0] in [re.split("\d+\.\d+\.\d+\.", a)[0] for a in installed_apps]:
+        print "update", app_name
+        updateApplication("{{ovp_tmpdir}}/%s" % app)
+        AdminConfig.save()
+    elif app not in [re.split("\d+\.\d+\.\d+\.", a)[0] for a in installed_apps]:
+        print "install", app, app_cl[app]
+        installApplication("{{ovp_tmpdir}}/%s" % app, [], app_cl[app], ["-appname", appname])
+        AdminConfig.save()
+        print "check that application %s is ready" % app
+        for time in range(1,2):
+            if isApplicationReady(appname):
+                break
+            else:
+                sleep(5)
+
+# start servers
+for cluster in ovp_clusters:
+    print "Start all servers in cluster %s" % cluster["name"]
+    startAllServersInCluster(cluster["name"])
+
+#startAllApplications()
